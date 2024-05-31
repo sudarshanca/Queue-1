611081b6bbcc6e2ea6cace3f42ea29037c8c0945
sudarshanca <sudarshanreddyca@gmail.com>
2024-05-31 14:14:36 +0530

    31-v2

---
+++ script.js
@@ -0,0 +1,6 @@
+function headerSidebar() {
+  let headerSidebarc = document.getElementById('header-sidebar')
+  let backgroundSidebar = document.getElementById('background-sidebar')
+  headerSidebarc.classList.toggle('show-header-sidebar');
+  backgroundSidebar.classList.toggle('show-background-sidebar');
+}
